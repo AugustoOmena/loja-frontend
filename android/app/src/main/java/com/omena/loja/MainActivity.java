1c947c68965a922267f763215809851881532067
Augusto do Nascimento Omena <augusto.n.omena@gmail.com>
2026-04-14 16:54:55 -0300

    feat: initialize Android project structure and integrate Capacitor
    - Added Android project files including build configurations, Gradle settings, and necessary resources for the Capacitor integration.
    - Created initial AndroidManifest.xml and MainActivity.java for app entry point.
    - Included various drawable resources for app icons and splash screens.
    - Updated package.json and package-lock.json to include Capacitor dependencies.
    - Introduced scripts for mobile build and synchronization with Capacitor.

---
+++ android/app/src/main/java/com/omena/loja/MainActivity.java
@@ -0,0 +1,5 @@
+package com.omena.loja;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.omena.loja',
+  appName: 'Loja Frontend',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+}
+
+export default config
